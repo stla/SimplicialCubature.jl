907d2e7831680ab240652ae290a70fc8a3bbbcfa
stla <laurent_step@yahoo.fr>
2022-12-05 11:08:55 +0100

    doc 1

--- docs/make.jl
+++ docs/make.jl
@@ -6,3 +6,7 @@ push!(LOAD_PATH, "../src/")
 using Documenter, SimplicialCubature
 
 makedocs(sitename = "SimplicialCubature.jl")
+
+deploydocs(
+    repo = "github.com/stla/SimplicialCubature.jl.git"
+)
