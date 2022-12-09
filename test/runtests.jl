d175a049d576a13e4f3e7cbafa50e4c79b99d459
stla <laurent_step@yahoo.fr>
2022-12-09 19:29:33 +0100

    new unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using SimplicialCubature
 using Test
 using TypedPolynomials
 
-@testset "SimplicialCubature.jl" begin
+@testset "A polynomial." begin
     function f(x)
         return x[1] + x[2]*x[3]
     end
@@ -13,5 +13,16 @@ using TypedPolynomials
     I_P = integratePolynomialOnSimplex(P, S)
     @test isapprox(
         I_f.integral, I_P
+    )
+end
+
+@testset "A function." begin
+    function f(x)
+        return exp(x[1] + x[2] + x[3])
+    end
+    S = [[0, 0, 0], [1, 1, 1], [0, 1, 1], [0, 0, 1]]
+    I_f = integrateOnSimplex(f, S)
+    @test isapprox(
+        I_f.integral, (exp(1) - 1)^3 / 6
     )
 end
